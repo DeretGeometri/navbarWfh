edb131998be2e27f096e11fec082c8a81b782155
Muhammad Taufik <51099044+DeretGeometri@users.noreply.github.com>
2022-08-19 22:15:56 +0700

    mengalihkan index

--- js/app.js
+++ js/app.js
@@ -7,3 +7,4 @@ const ul = document.querySelector(`ul`);
 function menuResponsif() {
   ul.classList.toggle(`responsif`);
 }
+window.location.replace("https://hop.clickbank.net/?affiliate=camlycast&vendor=socialpaid&pid=joblandingpage&ifso=40pertweet");
